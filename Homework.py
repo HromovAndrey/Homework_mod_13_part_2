cb72b5b8db11a36f60d0d18c8c55d0f050183850
user <andrey.gromov.36912@gmail.com>
2024-05-05 22:33:11 +0300

    Домашня робота

--- Homework.py
+++ Homework.py
@@ -1,49 +1,77 @@
-# Завдання 2
-# Створіть програму для проведення опитування або
-# анкетування. Зберігайте відповіді користувачів у форматі
-# JSON файлу. Кожне опитування може бути окремим
-# об'єктом у файлі JSON, а відповіді кожного користувача -
-# списком значень.
+# Завдання 3
+# До вже реалізованого класу «Стадіон» додайте можливість
+# стиснення та розпакування даних з використанням json та
+# pickle.
 
 import json
+import pickle
 
 
-def conduct_survey(question, answer_options):
-    print(question)
-    for i, version in enumerate(answer_options, 1):
-        print(f"{i}. {version}")
-    choice = int(input("Виберіть номер відповіді: "))
-    while choice not in range(1, len(answer_options) + 1):
-        choice = int(input("Невірний номер. Виберіть номер відповіді ще раз: "))
-    return choice
+class Stadion:
+    def __init__(self, name, city,capacity ):
+        self.name = name
+        self.city = city
+        self.capacity = capacity
+        self.employment = 0
 
+    def free_places(self):
+        return self.capacity - self.employment
 
-def save_answers(answers, filename):
-    with open(filename, 'w') as file:
-        json.dump(answers, file)
+    def зайняти_місця(self, number_seats):
+        if self.free_places() >= number_seats:
+            self.employment += number_seats
+            print(f"Місця на стадіоні {self.name} успішно зайнято.")
+        else:
+            print("На жаль, недостатньо вільних місць на стадіоні.")
 
+    def free_seats(self, number_seats):
+        if self.employment >= number_seats:
+            self.employment -= number_seats
+            print(f"Місця на стадіоні {self.name} успішно звільнено.")
+        else:
+            print("На стадіоні немає настільки зайнятих місць.")
 
-def download_survey(filename):
-    with open(filename, 'r') as f:
-        return json.load(f)
+    def save_json(self, filename):
+        with open(filename, 'w') as file:
+            json.dump(self.__dict__, file)
+
+    def download_json(self, filename):
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            self.__dict__.update(data)
+
+    def save_pickle(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.__dict__, file)
+
+    def download_pickle(self, filename):
+        with open(filename, 'rb') as file:
+            data = pickle.load(file)
+            self.__dict__.update(data)
+
+    def __str__(self):
+        return f"Стадіон '{self.name}' у місті {self.city}. Вмістимість: {self.capacity}, Зайнятість: {self.employment}"
 
 
 if __name__ == "__main__":
-    poll = {
-        "питання": "Яка ваша улюблена мова програмування?",
-        "варіанти_відповідей": ["Python", "Java", "JavaScript", "C++", "Інша"]
-    }
+    stadion = Stadion("Олімпійський", "Київ", 70000)
+    print("Початковий стан стадіону:")
+    print(stadion)
 
-    answers  = []
+    # Зберігаємо у JSON
+    stadion.save_json("stadion.json")
+    print("\nСтадіон збережено у JSON файлі.")
 
-    print("Вас вітає програма для проведення опитування!")
+    # Зберігаємо у pickle
+    stadion.save_pickle("stadion.pickle")
+    print("\nСтадіон збережено у pickle файлі.")
 
-    number_participants = int(input("Скільки учасників буде проходити опитування? "))
+    # Завантажуємо з JSON
+    stadion.download_json("stadion.json")
+    print("\nСтадіон завантажено з JSON файлу:")
+    print(stadion)
 
-    for member in range(1, number_participants + 1):
-        print(f"\nУчасник {member}:")
-        answers = conduct_survey(poll["питання"], poll["варіанти_відповідей"])
-        answers.append(answers)
-
-    save_answers(answers, "відповіді.json")
-    print("Ваші відповіді було збережено у файлі 'відповіді.json'.")
+    # Завантажуємо з pickle
+    stadion.download_pickle("stadion.pickle")
+    print("\nСтадіон завантажено з pickle файлу:")
+    print(stadion)
